b32aa28810cb35c048a70e8609ae1804434895ce
butch0012 <elnathanmburu@gmail.com>
2023-11-20 23:24:46 +0300

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,26 @@
+// Actors.js
 import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <>
+      <h1>Actors Page</h1>
+      {actors.map((actor, ind) => (
+        <div key={ind}>
+          <h3 data-testid="actor-name">Name: {actor.name}</h3>
+          <p>Movies:</p>
+          <ul>
+            {actor.movies.map((movie, ind) => (
+              <li key={ind} data-testid="movie">
+                {movie}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,20 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  const navStyle = {
+    display: "grid",
+  };
+
+  return (
+    <div className='navbar' style={navStyle}>
+      <NavLink to='/' exact>
+        Home
+      </NavLink>
+      <NavLink to='/Movies'>Movies</NavLink>
+      <NavLink to='/directors'>Directors</NavLink>
+      <NavLink to='/actors'>Actors</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
